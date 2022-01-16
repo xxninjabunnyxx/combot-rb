362593fd4e228a18d67e7811280eac61220b2253
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2022-01-15 21:19:33 -0500

    first commit

---
+++ lib/combot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "combot/version"
+
+module Combot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/combot/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Combot
+  VERSION = "0.0.3"
+end
